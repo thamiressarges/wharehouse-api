adf818cf9d0aa98fecca6d4e6c2188da6ce0e154
Thamires Sarges <thamires.sarges.58@gmail.com>
2026-06-19 13:45:05 -0400

    feat(database): configurar Entity Framework Core e AppDbContext

---
+++ wharehouse-api/Context/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using wharehouse_api.Models;
+
+namespace wharehouse_api.Context
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
