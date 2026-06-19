adaaad196b841bdf2963759a170caf3c2a7e6c94
Thamires Sarges <thamires.sarges.58@gmail.com>
2026-06-19 12:21:03 -0400

    feat(models): adicionar modelo de categoria

---
+++ wharehouse-api/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace wharehouse_api.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string?  ImageUrl { get; set; }
+    }
+}
