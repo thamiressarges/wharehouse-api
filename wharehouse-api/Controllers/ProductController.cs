3daa34aaa73addc1431f8d4d8bad5bb7319da47b
Thamires Sarges <thamires.sarges.58@gmail.com>
2026-06-21 14:08:11 -0400

    feat(api): implementar CRUD de produtos

---
+++ wharehouse-api/Controllers/ProductController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using wharehouse_api.Context;
+using wharehouse_api.Models;
+
+namespace wharehouse_api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProductController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> Get()
+        {
+            var products = _context.Products.ToList();
+            if(products is null)
+            {
+                return NotFound("Produtos não encontrados");
+            }
+            return products;
+        }
+
+        [HttpGet("{id:int}", Name="ObterProduto")]
+        public ActionResult<Product> Get(int id)
+        {
+            var product = _context.Products.FirstOrDefault(p => p.Id == id);
+            if(product is null)
+            {
+                return NotFound("Produto não encontrado");
+            }
+            return product;
+        }
+
+        [HttpPost]
+        public ActionResult Post(Product product)
+        {
+            if(product is null)
+            {
+                return BadRequest();
+            }
+            _context.Products.Add(product);
+            _context.SaveChanges();
+            return new CreatedAtRouteResult("ObterProduto", new { id = product.Id }, product);
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult Put(int id, Product product)
+        {
+            if(product.Id == id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(product).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return Ok(product);
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            var product = _context.Products.FirstOrDefault(p => p.Id == id);
+
+            if(product is null)
+            {
+                return NotFound("Produto não encontrado");
+            }
+
+            _context.Products.Remove(product);
+            _context.SaveChanges();
+            return Ok(product);
+        }
+    }
+}
+
