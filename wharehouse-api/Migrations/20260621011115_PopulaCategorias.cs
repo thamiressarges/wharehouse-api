8ac85f341e58bcb5b7d834950eaaf9a2c86e6729
Thamires Sarges <thamires.sarges.58@gmail.com>
2026-06-20 21:33:50 -0400

    feat(database): adicionar dados iniciais de categorias e produtos

---
+++ wharehouse-api/Migrations/20260621011115_PopulaCategorias.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace wharehouse_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulaCategorias : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+
+            mb.Sql("Insert into Categories(Name, ImageUrl) Values('Bebidas', 'bebidas.jpg')");
+            mb.Sql("Insert into Categories(Name, ImageUrl) Values('Lanches', 'lanches.jpg')");
+            mb.Sql("Insert into Categories(Name, ImageUrl) Values('Sobremesas', 'sobremesas.jpg')");
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("Delete from Categories");
+        }
+    }
+}
---
+++ wharehouse-api/Migrations/20260621011717_PopulaProdutos.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace wharehouse_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulaProdutos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("INSERT INTO Products(Name,Description,Price,ImageUrl,Stock,RegistrationDate,CategoryId) " +
+                    "VALUES('Coca-Cola Diet','Refrigerante de Cola 350 ml',5.45,'cocacola.jpg',50,NOW(),1)");
+
+            mb.Sql("INSERT INTO Products(Name,Description,Price,ImageUrl,Stock,RegistrationDate,CategoryId) " +
+                   "VALUES('Lanche de Atum','Lanche de Atum com maionese',8.50,'atum.jpg',10,NOW(),2)");
+
+            mb.Sql("INSERT INTO Products(Name,Description,Price,ImageUrl,Stock,RegistrationDate,CategoryId) " +
+                   "VALUES('Pudim 100 g','Pudim de leite condensado 100g',6.75,'pudim.jpg',20,NOW(),3)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("Delete from Products");
+        }
+    }
+}
