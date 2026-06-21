02d19eb9a7937b817247bfdaf48f7e7ff36e4892
Thamires Sarges <thamires.sarges.58@gmail.com>
2026-06-20 21:08:00 -0400

    feat(database): aplicar Data Annotations e atualizar esquema do banco

--- wharehouse-api/Models/Category.cs
+++ wharehouse-api/Models/Category.cs
@@ -1,7 +1,10 @@
 ﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace wharehouse_api.Models
 {
+    [Table("Categories")]
     public class Category
     {
 
@@ -10,8 +13,14 @@ namespace wharehouse_api.Models
             Products = new Collection<Product>();
         }
 
+        [Key]
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(80)]
         public string? Name { get; set; }
+
+        [StringLength(300)]
         public string?  ImageUrl { get; set; }
         public ICollection<Product>? Products { get; set; }
     }
--- wharehouse-api/Models/Product.cs
+++ wharehouse-api/Models/Product.cs
@@ -1,14 +1,31 @@
-﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace wharehouse_api.Models
 {
+    [Table("Products")]
     public class Product
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(80)]
         public string? Name { get; set; }
+
+        [Required]
+        [StringLength(300)]
         public string? Description { get; set; }
-        public double Price { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(10,2)")]
+        public decimal Price { get; set; }
+
+        [Required]
+        [StringLength(300)]
         public string? ImageUrl { get; set; }
+
         public double Stock { get; set; }
         public DateTime RegistrationDate { get; set; }
         public int CategoryId { get; set; }
