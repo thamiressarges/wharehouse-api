c13c8106c92000b90c2d2123fc647cbbe3c469d2
Thamires Sarges <thamires.sarges.58@gmail.com>
2026-06-22 11:25:51 -0400

    fix(api): configurar serialização JSON para ignorar referências cíclicas

--- wharehouse-api/Program.cs
+++ wharehouse-api/Program.cs
@@ -1,9 +1,11 @@
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 using wharehouse_api.Context;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers().AddJsonOptions(options => options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
